45c964c5ad8001df95ea47a099d87cd85a824e73
Junya <remiki.anis3@gmail.com>
2022-06-20 01:48:55 +0200

    1/ex00/

---
+++ 1/ex00/.hpp
@@ -0,0 +1,17 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+   public:
+      Zombie();
+      ~Zombie();
+      void  announce();
+   private:
+      std::string name;
+};
+
+#endif
