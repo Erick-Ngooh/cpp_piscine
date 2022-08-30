b96af1412a6402b01e4fe4a134d092c936d1d2d7
Anis Remiki <anremiki@student.42.fr>
2022-08-31 00:15:51 +0200

    .

--- 7/ex02/Array.hpp
+++ 7/ex02/Array.hpp
@@ -10,20 +10,57 @@ class Array
 	public:
 		Array(void){
 			_array = NULL;
+			_size = 0;
 		}
 		Array(unsigned int n){
 			if (n)
-				_array = new T(n);
+			{
+				_array = new T[n];
+				_size = n;
+			}
 			else
+			{
 				_array = NULL;
+				_size = 0;
+			}
 		}
-		Array(T const &src){
-			_array = new T(src.size());
-			for (int i = 0; src._array[i]; i++)
-				_array[i] = src._array[i];
+		Array(Array const &src){
+			*this = src;
+		}
+		~Array(void){
+			if (_size)
+				delete [] _array;
+		}
+		unsigned int size( void ){
+			return _size;
+		}
+		class Overflow : public std::exception{
+			public:
+				virtual const char *what() const throw(){
+					return "Possible buffer overflow";
+				}
+		};
+		Array &operator=(Array const &src){
+			if (_size)
+				delete [] _array;
+			if (src._size)
+			{
+				_array = new T[src._size];
+				for (int i = 0; src._array[i]; i++)
+					_array[i] = src._array[i];
+			}
+			_size = src._size;
+			return *this;
+		}
+		T & operator[]( unsigned int index ){
+			if (!_size || index >= _size)
+				throw(Overflow());
+			else
+				return _array[index];
 		}
 	private:
-		T	_array;
+		T	*_array;
+		unsigned int	_size;
 };
 
 #endif
