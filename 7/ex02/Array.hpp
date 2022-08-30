7f9e3cfcbe6774fbae0e185135ae6fe76d7d16ed
Anis REMIKI <anremiki@e2r8p2.clusters.42paris.fr>
2022-08-30 20:02:24 +0200

    update 7 ex02

--- 7/ex02/Array.hpp
+++ 7/ex02/Array.hpp
@@ -4,7 +4,26 @@
 #include <iostream>
 #include <exception>
 
+template<typename T>
 class Array
 {
+	public:
+		Array(void){
+			_array = NULL;
+		}
+		Array(unsigned int n){
+			if (n)
+				_array = new T(n);
+			else
+				_array = NULL;
+		}
+		Array(T const &src){
+			_array = new T(src.size());
+			for (int i = 0; src._array[i]; i++)
+				_array[i] = src._array[i];
+		}
+	private:
+		T	_array;
+};
 
-}
+#endif
